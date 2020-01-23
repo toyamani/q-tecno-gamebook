e999ed4cdf18fe3ecd725c843dde904892fdf776
To-ya Yamanishi <neotyhdpc@gmail.com>
2020-01-23 12:01:14 +0900

    message chage

--- server.js
+++ server.js
@@ -36,10 +36,84 @@ function handleEvent(event) {
   }
   
   console.log('replayMessage');
-  return client.replyMessage(event.replyToken, {
-    type: 'text',
-    text: event.message.text //実際に返信の言葉を入れる箇所
-  });
+  const message = {
+    "type": "template",
+    "altText": "this is a carousel template",
+    "template": {
+        "type": "carousel",
+        "columns": [
+            {
+              "thumbnailImageUrl": "https://example.com/bot/images/item1.jpg",
+              "imageBackgroundColor": "#FFFFFF",
+              "title": "this is menu",
+              "text": "description",
+              "defaultAction": {
+                  "type": "uri",
+                  "label": "View detail",
+                  "uri": "http://example.com/page/123"
+              },
+              "actions": [
+                  {
+                      "type": "postback",
+                      "label": "Buy",
+                      "data": "action=buy&itemid=111"
+                  },
+                  {
+                      "type": "postback",
+                      "label": "Add to cart",
+                      "data": "action=add&itemid=111"
+                  },
+                  {
+                      "type": "uri",
+                      "label": "View detail",
+                      "uri": "http://example.com/page/111"
+                  }
+              ]
+            },
+            {
+              "thumbnailImageUrl": "https://example.com/bot/images/item2.jpg",
+              "imageBackgroundColor": "#000000",
+              "title": "this is menu",
+              "text": "description",
+              "defaultAction": {
+                  "type": "uri",
+                  "label": "View detail",
+                  "uri": "http://example.com/page/222"
+              },
+              "actions": [
+                  {
+                      "type": "postback",
+                      "label": "Buy",
+                      "data": "action=buy&itemid=222"
+                  },
+                  {
+                      "type": "postback",
+                      "label": "Add to cart",
+                      "data": "action=add&itemid=222"
+                  },
+                  {
+                      "type": "uri",
+                      "label": "View detail",
+                      "uri": "http://example.com/page/222"
+                  }
+              ]
+            }
+        ],
+        "imageAspectRatio": "rectangle",
+        "imageSize": "cover"
+    }
+  } 
+'  return client.replyMessage(event.replyToken, {
+'    type: 'text',
+'    text: event.message.text //実際に返信の言葉を入れる箇所
+'  }
+  return client.replyMessage(evnet.replyToken, message)
+    .then(() => {
+      callback(null, {});
+    })
+    .catch((error) => {
+        callback(null, {});
+    });
 }
 
 app.listen(PORT);
