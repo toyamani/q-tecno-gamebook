face4205de9e7716e66d8e7d11e5871f86bce772
To-ya Yamanishi <neotyhdpc@gmail.com>
2020-02-26 11:17:05 +0900

    ADD: images faile, true mail image

--- server.js
+++ server.js
@@ -103,19 +103,11 @@ const handleEvent = (event) => {
 app.get('/', (req, res) => res.send('Hello LINE BOT!(GET)')); // ブラウザ確認用(無くても問題ない)
 app.post('/webhook', line.middleware(config), (req, res) => {
   console.log(req.body.events);
-
-  // ここのif分はdeveloper consoleの'接続確認'用なので削除して問題ないです。
-  if (req.body.events[0].replyToken === '00000000000000000000000000000000' && req.body.events[1].replyToken === 'ffffffffffffffffffffffffffffffff') {
-    res.send('Hello LINE BOT!(POST)');
-    console.log('疎通確認用');
-    return;
-  }
-
+  
   Promise
     .all(req.body.events.map(handleEvent))
     .then((result) => res.json(result));
 });
-
 
 app.listen(PORT);
 console.log(`Server running at ${PORT}`);
